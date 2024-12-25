57de34fe1f16fa4439e69a9bc9dca7668f80ee82
Katsuhiro Shiraishi <sc30.katsu@gmail.com>
2024-12-25 17:06:52 +0900

    .feat add pokemon feature

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,10 +1,12 @@
 import { booksTypeDefs, booksResolvers } from "@/resolvers/books.ts";
 import { authorsTypeDefs, authorsResolvers } from "@/resolvers/authors.ts";
+import { pokemonResolvers, pokemonTypeDefs } from "@/resolvers/pokemon.ts";
 
-export const typeDefs = [booksTypeDefs, authorsTypeDefs];
+export const typeDefs = [booksTypeDefs, authorsTypeDefs, pokemonTypeDefs];
 export const resolvers = {
   Query: {
     ...booksResolvers,
     ...authorsResolvers,
+    ...pokemonResolvers,
   },
 };
